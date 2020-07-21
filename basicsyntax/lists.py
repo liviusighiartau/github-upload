7d6588c0634259af038f9192fd58281db046c77d
liviusighiartau <sighiartauliviu@gmail.com>
2020-07-21 16:48:41 +0300

    Add a file for lists learning

---
+++ basicsyntax/lists.py
@@ -0,0 +1,26 @@
+"""
+Data type you store more than one value in one variable name
+List items are in brackets, separated with "," [1, 2, 3]
+"""
+
+cars = ["bmw", "audi", "vw"]
+empty_list = []
+print(empty_list)
+print(cars)
+
+print("*#"*20)
+
+print(cars[0])
+
+num_list = [1, 2, 3]
+sum_num = num_list[0] + num_list[2]
+
+print(sum_num)
+
+more_cars = ["bmw", "audi", "vw"]
+print(more_cars[0])
+
+more_cars[1] = "benz"
+print(more_cars[1])
+
+print(more_cars)
