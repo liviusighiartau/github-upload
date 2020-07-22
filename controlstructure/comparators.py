6ce38be376daf53b4ede088a0d9e9527e8cf3d80
liviusighiartau <sighiartauliviu@gmail.com>
2020-07-22 09:50:57 +0300

    Create control structure folder with some basic comparators file in it

---
+++ controlstructure/comparators.py
@@ -0,0 +1,16 @@
+"""
+== --> Value equality
+!= --> Not equal to
+<  --> Less than
+>  --> Greater than
+<- --> Less than or equal to
+>= --> Greater than or equal to
+"""
+
+bool_one = 10 == 10
+not_equal = 10 != 11
+less_than = 10 < 100
+greater_than = 10 > 1
+lt_equal = 10 <= 10
+gt_equal = 10 >= 10 - 1
+print(gt_equal)
