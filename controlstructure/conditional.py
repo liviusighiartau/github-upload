98abab1287c59160af8cdbbe4e793d12fcc1aa9e
liviusighiartau <sighiartauliviu@gmail.com>
2020-07-22 10:42:25 +0300

    Basic IF, ELIF

---
+++ controlstructure/conditional.py
@@ -0,0 +1,20 @@
+"""
+Conditional logic
+"""
+
+if 1 > 0:
+    print("One is greater than 0")
+
+value = 'green'
+
+if value == 'green':
+    print("Let's go!")
+else:
+    print("Let's stop!")
+
+if value == 'red':
+    print("You have to stop at this point.")
+elif value == 'green':
+    print("You can run as fast as you want.")
+
+print("Let's code it!")
